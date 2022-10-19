3a928721a5ebe5417dd38539c60ed1c6255cef56
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-19 08:00:41 +0000

    write start function and main function for testing

--- run.py
+++ run.py
@@ -13,5 +13,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('quarterback_performance')
 
 user_input = SHEET.worksheet('input')
-data = user_input.get_all_values()
-print(data)
+
+
+def start():
+    """
+    Welcomes the user to the interface.
+    Explains the benefits of using the app.
+    """
+    print("Welcome to the quarterback performance app") 
+    print("- The source for individual quarterback ratings\n")
+    print("Here you can enter the statistics of the match day...")
+    print("and see exactly how your favorite players performed.\n")
+    print("################\n")
+
+
+def main():
+    """
+    This function calls all necessary functions in the right order, 
+    according to the flow chart in the doc.
+    """
+    start()
+
+
+main()
