e1f625417c028fb64e4e3548c7066a99d580b131
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-19 10:11:08 +0000

    test grabbing of values in check function and change get_quarterback function

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('quarterback_performance')
 
 user_input = SHEET.worksheet('input')
+averages = SHEET.worksheet("averages")
 
 
 def start():
@@ -30,32 +31,37 @@ def start():
 def get_quarterback():
     """
     Gets the name of the quarterback from the user.
-    Checks if it is a string value.
+    Capitalizes the first letter of the lastname.
     """
     name = input("Enter the lastname of the quarterback here: \n")
-    name.title()
-
-    if isinstance(name, str):
-        print("Perfect, the lastname is valid!")
-    else:
-        print("The input must contain a combination of letters")
-        get_quarterback()
+    name = name.title()
     return name
-
 
 def get_gameday():
     """
     Gets the gameday integer from the user.
     """
     print("A football season has 17 game days for each team.")
-    gameday = input("Enter the current gameday number(from 1 to 17)\n")
+    gameday = int(input("Enter the current gameday number(from 1 to 17)\n"))
 
     try: 
-        if type(gameday) != "str" or gameday > 17 or gameday < 1:
+        if gameday > 17 or gameday < 1:
             raise ValueError("The number entered is not correct")
     except ValueError as e:
         print(f"Invalid value: {e}, please enter a number between 1 and 17.\n")
         get_gameday()
+    else:
+        return gameday
+
+
+def check(name, gameday):
+    """
+    This function checks, if the gameday already exists in the spreadsheet.
+    """
+    existing_names = user_input.col_values(8)
+    existing_gamedays = user_input.col_values(9)
+
+    print(existing_names)
 
 
 #def check_integer(value):
@@ -74,7 +80,7 @@ def main():
     start()
     name = get_quarterback()
     gameday = get_gameday()
-
+    check()
 
 
 main()
