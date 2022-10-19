d6f0655731083a6e19f5cd90ae0139b606acb102
thomasstrassmann <thomas_strassmann@freenet.de>
2022-10-19 08:21:10 +0000

    write get_quarterback function

--- run.py
+++ run.py
@@ -27,12 +27,29 @@ def start():
     print("################\n")
 
 
+def get_quarterback():
+    """
+    Gets the name of the quarterback from the user.
+    Checks if it is a string value.
+    """
+    name = input("Enter the lastname of the quarterback here: \n")
+    name.title()
+
+    if type(name) == "str":
+        print("Perfect, the lastname is valid!")
+    else:
+        print("The input must contain a combination of letters")
+        get_quarterback()
+    return name
+
+
 def main():
     """
     This function calls all necessary functions in the right order, 
     according to the flow chart in the doc.
     """
     start()
+    name = get_quarterback()
 
 
 main()
